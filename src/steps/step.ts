a1d0b92ef6684323539f4d883a2b954fc91b32d8
Tom Bogin <tombo@monday.com>
2024-03-19 18:41:14 +0200

    reload after install

--- src/steps/step.ts
+++ src/steps/step.ts
@@ -1,19 +1,35 @@
+import { runCommand } from "lib/utils/exec";
+
 export abstract class Step {
-    async install(): Promise<{success: boolean, reason?: string, error?: Error}> {
-        try {
-            await this.installStep();
-        } catch(e: any) {
-            return { success: false, error: e, reason: `Error during installation. Error: ${e.message}` }
-        }
-        const isInstalledNow = await this.installCheck();
-        return { success: isInstalledNow.valid, reason: isInstalledNow.reason};
+  async install(): Promise<{
+    success: boolean;
+    reason?: string;
+    error?: Error;
+  }> {
+    try {
+      await this.installStep();
+      // reload env variables
+      await runCommand(`source ~/.zshrc > /dev/null 2>&1 || true`);
+    } catch (e: any) {
+      return {
+        success: false,
+        error: e,
+        reason: `Error during installation. Error: ${e.message}`,
+      };
     }
+    const isInstalledNow = await this.installCheck();
+    return { success: isInstalledNow.valid, reason: isInstalledNow.reason };
+  }
 
-    async checkPreinstall(): Promise<{valid: boolean, reason?: string, fixAction?: () => Promise<void> }> {
-        return { valid: true, reason: "Can install" };
-    }
+  async checkPreinstall(): Promise<{
+    valid: boolean;
+    reason?: string;
+    fixAction?: () => Promise<void>;
+  }> {
+    return { valid: true, reason: "Can install" };
+  }
 
-    abstract installCheck(): Promise<{valid: boolean, reason?: string }>;
-    abstract installStep(): Promise<void>;
-    abstract name(): string;
+  abstract installCheck(): Promise<{ valid: boolean; reason?: string }>;
+  abstract installStep(): Promise<void>;
+  abstract name(): string;
 }
