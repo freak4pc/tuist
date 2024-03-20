61ff209dcb75bbe1ba5f0efe3e377d65ffba3993
Tom Bogin <tombo@monday.com>
2024-03-20 12:34:55 +0200

    fix clone try

--- src/basicSteps.ts
+++ src/basicSteps.ts
@@ -7,45 +7,60 @@ import { InstallNvm } from "./steps/prepare/nvm";
 import { InstallPipPackages } from "./steps/prepare/pip-packages";
 import { SetEnvVariable } from "./steps/prepare/setenv";
 import { XcodeInstall } from "./steps/prepare/xcode";
-import { NpmAuth } from './steps/auth/npmauth';
-import { BrewAddRepos } from './steps/prepare/brew-add-repos';
-import { InstallOkteto } from './steps/prepare/install-okteto';
-import { AwsCreds } from './steps/prepare/awscreds';
-import { PythonFixes } from './steps/prepare/pythonfixes';
-import { InstallTeleport } from './steps/prepare/teleport';
-import { GitConfig } from './steps/config/gitconfig';
-import { GithubAuth } from './steps/auth/githubauth';
-import { InstallNpmPackages } from './steps/prepare/npmpackages';
-import { GitClone } from './steps/config/gitclone';
-import { DotfilesConfig } from './steps/config/dotfiles-config';
-import { RvmInstaller } from './steps/prepare/rvm';
-import { Step } from './steps/step';
+import { NpmAuth } from "./steps/auth/npmauth";
+import { BrewAddRepos } from "./steps/prepare/brew-add-repos";
+import { InstallOkteto } from "./steps/prepare/install-okteto";
+import { AwsCreds } from "./steps/prepare/awscreds";
+import { PythonFixes } from "./steps/prepare/pythonfixes";
+import { InstallTeleport } from "./steps/prepare/teleport";
+import { GitConfig } from "./steps/config/gitconfig";
+import { GithubAuth } from "./steps/auth/githubauth";
+import { InstallNpmPackages } from "./steps/prepare/npmpackages";
+import { GitClone } from "./steps/config/gitclone";
+import { DotfilesConfig } from "./steps/config/dotfiles-config";
+import { RvmInstaller } from "./steps/prepare/rvm";
+import { Step } from "./steps/step";
 
 export const basicSteps: Step[] = [
-    new CreateDevelopmentFolder(),
-    new XcodeInstall(),
-    new InstallHomebrew(),
-    new InstallIterm(),
-    new InstallGit(),
-    new InstallNvm(),
-    new InstallPipPackages(["virtualenv"]),
-    new InstallPipPackages(["octodns==0.9.10", "nsone", "boto3", "route53"]),
-    new InstallBrewPackages(["cask", "make", "yarn"]),
-    new InstallBrewPackages(["iterm2", "jq", "gpg", "kubectl", "kubectx", "warrensbox/tap/tfswitch", "aws-iam-authenticator", "pyenv", "awscli", "redis", "mysql"], { force: true }),
-    new InstallBrewPackages(["gh"]),
+  new CreateDevelopmentFolder(),
+  new XcodeInstall(),
+  new InstallHomebrew(),
+  new InstallIterm(),
+  new InstallGit(),
+  new InstallNvm(),
+  new InstallPipPackages(["virtualenv"]),
+  new InstallPipPackages(["octodns==0.9.10", "nsone", "boto3", "route53"]),
+  new InstallBrewPackages(["cask", "make", "yarn"]),
+  new InstallBrewPackages(
+    [
+      "iterm2",
+      "jq",
+      "gpg",
+      "kubectl",
+      "kubectx",
+      "warrensbox/tap/tfswitch",
+      "aws-iam-authenticator",
+      "pyenv",
+      "awscli",
+      "redis",
+      "mysql",
+    ],
+    { force: true }
+  ),
+  new InstallBrewPackages(["gh"]),
 
-    new PythonFixes(),
+  new PythonFixes(),
 
-    new SetEnvVariable("AWS_PROFILE", "default"),
-    new SetEnvVariable("AWS_REGION", "us-east-1"),
-    new InstallBrewPackages(["dbeaver-community"], { cask: true }),
+  new SetEnvVariable("AWS_PROFILE", "default"),
+  new SetEnvVariable("AWS_REGION", "us-east-1"),
+  new InstallBrewPackages(["dbeaver-community"], { cask: true }),
 
-    new BrewAddRepos(["codefresh-io/cli", "versent/taps"]),
-    new InstallBrewPackages(["codefresh", "saml2aws"]),
-    new AwsCreds(),
-    new InstallOkteto(),
-    // new RvmInstaller(),
-    /*
+  new BrewAddRepos(["codefresh-io/cli", "versent/taps"]),
+  new InstallBrewPackages(["codefresh", "saml2aws"]),
+  new AwsCreds(),
+  new InstallOkteto(),
+  // new RvmInstaller(),
+  /*
     Need to add these:
 
     # installing Ruby - version 2.7.2
@@ -58,14 +73,14 @@ export const basicSteps: Step[] = [
     brew reinstall openssl@3
     rvm --default use 2.7.2
     */
-    new InstallTeleport(),
-    new NpmAuth(),
+  new InstallTeleport(),
+  new NpmAuth(),
 
-    new GitConfig(),
-    new GithubAuth(),
+  new GitConfig(),
+  new GithubAuth(),
 
-    new InstallNpmPackages(["@mondaydotcomorg/monday-dev-cli"]),
+  new InstallNpmPackages(["@mondaydotcomorg/monday-dev-cli"]),
 
-    new GitClone("DaPulse/dotfiles", "~/dotfiles"),
-    new DotfilesConfig()
+  new GitClone("org-Dapulse@github.com:DaPulse/dotfiles.git", "~/dotfiles"),
+  new DotfilesConfig(),
 ];
