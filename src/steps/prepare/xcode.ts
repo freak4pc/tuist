f06598f0a7dc41b0babb9892746d31736867a904
Shai Mishali <freak4pc@example.com>
2024-03-20 15:47:32 +0200

    Wait for Xcode CLI

--- src/steps/prepare/xcode.ts
+++ src/steps/prepare/xcode.ts
@@ -1,3 +1,4 @@
+import { pressAnyKeyToContinue } from 'lib/utils/question';
 import { Step } from '../step';
 import { runCommand } from 'lib/utils/exec';
 
@@ -8,24 +9,26 @@ export class InstallXcodeCLI extends Step {
     async installCheck() {
         try {
             if(!(await runCommand('xcode-select -p')).trim()) {
-                return { valid: false, reason: "Xcode is not installed. Command not found" };
+                return { valid: false, reason: "Xcode command line tools are not installed. Command not found" };
             }
         } catch(e: any) {
             if (e.message.includes("command not found")) {
-                return { valid: false, reason: "Xcode is not installed. Command not found" };
+                return { valid: false, reason: "code command line tools are not installed. Command not found" };
             }
             return { valid: false, reason: `Xcode error while checking. ${e.message}` };
         }
         
-        return { valid: true, reason: "Already installed xcode, able to run xcode-select" };
+        return { valid: true, reason: "Already installed Xcode Command Line Tools" };
     }
 
     name() {
-        return "Install Xcode";
+        return "Install Xcode Command Line Tools";
     }
 
     async installStep() {
-        console.log("Installing xcode")
+        console.log("Installing Xcode Command Line Tools")
         await runCommand('xcode-select --install');
+        console.log("An installer window should now open. Please click the \"Install\" button to install the command line tools.\nPress any key when the installation is done.")
+        await pressAnyKeyToContinue();
     }
 }
